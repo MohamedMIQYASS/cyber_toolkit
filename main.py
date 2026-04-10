91833ee5502f6f38fa0f726fa3afdabdb3387178
MohamedMIQYASS <mohamedmiqyass@outlook.fr>
2026-04-10 22:14:17 +0200

    a

--- main.py
+++ main.py
@@ -10,9 +10,25 @@ label = Label(fenetre,text="Cyber-Toolkit")
 label.pack()
 
 #titre principal
-
 titrep = Label(fenetre , text="CYBER TOOLKIT" , fg="white", font=("Arial",70) , bg=fenetre["bg"])
+titrep.pack(expand=True)
 titrep.pack()
+
+#saisie du login et du mdp
+Champlogin = Text(fenetre, height=1, width=50)
+titreLogin = Label(fenetre , text="votre login" , fg="white", font=("Arial",10) )
+Champlogin.pack()
+ChampMdp = Text(fenetre, height=1, width=50)
+titreMdp = Label(fenetre , text="votre mdp" , fg="white", font=("Arial",10) , bg=fenetre["bg"])
+ChampMdp.pack()
+
+
+
+
+
+
+
+
 
 
 
